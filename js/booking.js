5a6c412a77617c75b52f5ffc4b51c82294805be4
Smyrutskyi <myruckyy@mail.ru>
2017-09-06 17:09:33 +0300

    sergey
    213

--- js/booking.js
+++
@@ -1,7 +0,0 @@
-var search = document.querySelector(".search-btn");
-var formbody = document.querySelector(".form");
-
-search.addEventListener("click", function(event) {
-  event.preventDefault();
-  formbody.classList.toggle("form-show");
-});
